3ff5c11aaf1c39c707f08537defaf28f08085a9e
Guilherme Santos <guipinela.santos@gmail.com>
2021-10-11 23:10:19 +0100

    commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
 from PySide6.QtWidgets import QApplication, QPushButton, QWidget, QVBoxLayout, QMainWindow, QMenu, QMenuBar
-from PySide6.QtGui import QIcon, QKeySequence
+from PySide6.QtGui import QIcon, QKeySequence, QAction
 
 
-def say_hello():
+def say_hello(*args):
     print("Button clicked, Hello!")
+    print(args)
 
 
 class GUI(QMainWindow):
@@ -20,9 +21,16 @@ class GUI(QMainWindow):
         menuBar = self.menuBar()
         filemenu = QMenu('File', self)
         menuBar.addMenu(filemenu)
-        filemenu.addAction(QIcon('novodocumento.ico'), 'New')
-        filemenu.addAction(QIcon('abrirdocumento.ico'), 'Open')
-        filemenu.addAction(QIcon('salvar.ico'), 'Save')
+        botao_new = filemenu.addAction(QIcon('novodocumento.ico'), 'New')
+        # botao_new.triggered.connect(lambda: say_hello(self))
+        botao_open = filemenu.addAction(QIcon('abrirdocumento.ico'), 'Open')
+        # botao_open.triggered.connect(lambda: ())
+        botao_save = filemenu.addAction(QIcon('salvar.ico'), 'Save')
+        botao_save.triggered.connect(lambda: self.salvar_arquivo())
+
+    def salvar_arquivo(self, path, text):
+        with open(path, 'w') as filetext:
+            file = filetext.write(text)
 
 
 if __name__ == '__main__':
